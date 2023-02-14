1904151303159b82f92acc9dba96823e50d8464f
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-14 16:54:31 +0200

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,9 @@
+#create a variable and set that variable to "Super Man"
+hero = "Super Man"
+#create the vraiable to upper case
+hero1 = hero.upper()
+#create anpther variable to be able to replace the space to a "^" character
+hero2 = hero1.replace(" " , "^")
+#print out the results whiles also adding the "^" after every word
+print(hero2[0] + "^" + hero2[1] + "^" + hero2[2] + "^" +  hero2[3] + "^" +  hero2[4] + "^" + hero2[5] + hero2[6] + "^" + hero2[7] + "^" + hero2[8])
+
